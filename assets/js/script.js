2af6973599b36ad04f9524e1b8e5bea2c18e5425
Jonah <jonahtkane@gmail.com>
2023-01-29 20:06:38 -0600

    updates to everything.... not working correctly yet

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
-   
-//lat and long data,
-//http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+// let currentDate = dayjs();
+// $('#current-date').text(currentDate.format('dddd, MMMM DD'));
 
-let apiKey = "c98e2acdd96b3a859a933d44be6055e7"
+
+let apiKey = "26aab995b68ef9eaae3c5d668d386911";
 
 document.getElementById("search-btn").addEventListener("click", function(event) {
     event.preventDefault()
@@ -22,19 +22,92 @@ return response.json()
         console.log(data);
         let lat = data[0].lat
         let lon = data[0].lon
-        fiveDayWeather(lat, lon);
+        currentForecast, fiveDayWeather (lat, lon);
     })
 
 }
-
-function fiveDayWeather (lat, lon) {
-    let weatherAPI = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`;
+function currentForecast(lat, lon) {
+    let weatherAPI = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`;
     fetch(weatherAPI)
     .then(function(response){
         return response.json()
     })
     .then(function(data){
         console.log(data);
-    })
 
-}
+        let location = data.name;
+        let temp = data.main.temp
+        let humidity = data.main.humidity;
+        let wind = data.wind.speed; 
+        let degSymbol = `\u00B0`;
+        let currentDate = dayjs(); 
+        let description = data.weather[0].description;
+        let icon = data.weather[0].icon;
+        let weatherIcon = "http://openweathermap.org/img/wn/" + icon + ".png";
+         
+        console.log("The weather for today in " + location + " is: " + "Temperature: " + Math.floor(temp) + degSymbol + "F", 
+        "Humidity: "+ humidity + "%",
+        "Wind speed: " +Math.floor(wind) + "MPH", 
+        icon,
+        weatherIcon,
+        "Current Conditions: " + description);
+    
+        $('#forecast-info').text("Forecast info for ");
+        $('#current-date').text(currentDate.format('dddd, MMMM DD'));
+        $('#location').text("In: " + location);
+        document.querySelector(".icon").src = weatherIcon;
+        $('#temp').text("Temperature: " + Math.floor(temp) + degSymbol + "F");
+        $('#humidity').text("Humidity: "+ humidity + "%");
+        $('#wind').text("Wind speed: " +Math.floor(wind) + " MPH");
+        $('#description').text("Current conditions: " + description);
+    })}
+    //////////////////////////////////
+function fiveDayWeather (lat, lon) {
+                let weatherAPI = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`;
+                fetch(weatherAPI)
+                .then(function(response){
+                    return response.json()
+                })
+                .then(function(data){
+                    console.log(data);
+                    let location = data.city.name;
+                    let futureDate = data.list[0].dt_txt;
+                    let temp = data.list[0].main.temp;
+                    let humidity = data.list[0].main.humidity;
+                    let wind = data.list[0].wind.speed; 
+                    let degSymbol = `\u00B0`;
+                    let description = data.list[0].weather[0].description;
+                    let icon = data.list[0].weather[0].icon;
+                    let weatherIcon = "http://openweathermap.org/img/wn/" + icon + ".png";
+                    
+                    
+                    console.log("The weather for " + futureDate + "in " + location + " is: " + "Temperature: " + Math.floor(temp) + degSymbol + "F", 
+                    "Humidity: "+ humidity + "%",
+                    "Wind speed: " +Math.floor(wind) + "MPH", 
+                    icon,
+                    weatherIcon,
+                    "Current Conditions: " + description);
+                    
+                })
+            }
+            let day0 = dayjs();
+            let day1 = day0.add(1, 'day');
+            let day2 = day0.add(2, 'day');
+            let day3 = day0.add(3, 'day');
+            let day4 = day0.add(4, 'day');
+            let day5 = day0.add(5, 'day');
+            // console.log(day0.format('dddd, MMMM DD')) 
+            // console.log(day1.format('dddd, MMMM DD')) 
+            // console.log(day2.format('dddd, MMMM DD'))
+            // console.log(day3.format('dddd, MMMM DD'))
+            // console.log(day4.format('dddd, MMMM DD'))
+            // console.log(day5.format('dddd, MMMM DD'))
+            for (let i = 0; i <5; i++) {
+    // let APIDate = data.list[0].dt_txt;
+            $('#day1').text(day1.format('dddd, MMMM DD'));
+            $('#day2').text(day2.format('dddd, MMMM DD'));
+            $('#day3').text(day3.format('dddd, MMMM DD'));
+            $('#day4').text(day4.format('dddd, MMMM DD'));
+            $('#day5').text(day5.format('dddd, MMMM DD'));
+            }
+            // console.log(APIDate);
